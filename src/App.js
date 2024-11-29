f92aeca2cb5b9deb323396903ae0c5d6fbaf1379
Nbr Noof <nbrnooof9087@gmail.com>
2024-11-29 10:30:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './components/Header';
+import './components/style.css'
+const App = () => {
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+    <Header/>
+    <div className='main'>
+      <div className='content'>
+      <h2>You deserve to be happy</h2>
+      <h3>How A Therapy Chatbot Can<br></br> Provide Essential Healthcare Services</h3>
+      <a  
+      className="btn"
+      href="https://novathon.vercel.app/chat"
+    >
+      Let's start
+    </a>
+      </div>
+      <div className='bodyimg'><img src='bodyimg1.png'></img></div>
     </div>
+  </div>
+  </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.css'; // You can create a separate CSS file for styling
+
+const Header = () => {
+  return (
+    <card>
+    <header className="header">
+      <img src="bmlogo2.png" alt="Logo" className="logo"/>
+      <h1 className="title">BetterMind</h1>
+      <nav>
+        <ul className="navList">
+          <li className="navItem"><a href="/" className="navLink">Home</a></li>
+          <li className="navItem"><a href="/https://novathon.vercel.app/login" className="navLink">Login</a></li>
+        </ul>
+      </nav>
+    </header></card>
+  );
+};
+
+export default Header;
